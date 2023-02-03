2fcbf1759385f7e5cf7ddb134e6d8619f72b2203
Charlie <charlie3.thomas@live.uwe.ac.uk>
2023-02-03 19:22:36 +0000

    [GGJ23_Roots] AI Agent Aggro Range and switching behaviour

--- Assets/AI/EnemyNavMesh.cs
+++ Assets/AI/EnemyNavMesh.cs
@@ -1,21 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyNavMesh : MonoBehaviour
 {
 
-    [SerializeField] private Transform target_location;
+    private enum State
+    {
+        ROAM,
+        ACTIVE,
+        NONE
+    }
+
+
     [SerializeField] private UnityEngine.AI.NavMeshAgent nav_mesh_agent;
+    [SerializeField] private Transform ai_target_player;
+    [SerializeField] private float ai_aggro_range = 0;
+    [SerializeField] private State ai_state = State.NONE;
 
     private void Awake()
     {
         nav_mesh_agent = GetComponent<NavMeshAgent>();
+        if (ai_aggro_range == 0)
+        {
+            Debug.LogWarning("AI Agent \"" + this.name + "\" has aggro range of Zero.");
+        }
     }
 
     private void Update()
     {
-        nav_mesh_agent.destination = target_location.position;
+        Debug.Log(CheckPlayerInRange());
+        if (CheckPlayerInRange())
+        {
+            ai_state = State.ACTIVE;
+        }
+        else
+        {
+            ai_state = State.ROAM;
+        }
+
+        Navigate();
+    }
+
+    private bool CheckPlayerInRange()
+    {
+        if (Vector3.Distance(this.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < ai_aggro_range)
+            return true;
+        return false;
+    }
+
+    private void Navigate()
+    {
+        switch(ai_state)
+        {
+            case State.NONE:
+                nav_mesh_agent.destination = this.transform.position;
+                break;
+            case State.ROAM:
+                nav_mesh_agent.destination = this.transform.position;
+                break;
+            case State.ACTIVE:
+                ai_target_player = GameObject.FindGameObjectWithTag("Player").transform;
+                nav_mesh_agent.destination = ai_target_player.position;
+                break;
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw Sphere to indicate agent aggro range
+        Gizmos.DrawWireSphere(this.transform.position, ai_aggro_range);
     }
 }
