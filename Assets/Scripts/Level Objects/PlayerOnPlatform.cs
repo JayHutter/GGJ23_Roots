45dd6bd09af42c3a9eb9e811fc43a324030a050b
Ben Williams <43639041+BenWilliams1@users.noreply.github.com>
2023-02-04 16:48:50 +0000

    Adding more parts to the level

---
+++ Assets/Scripts/Level Objects/PlayerOnPlatform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOnPlatform : MonoBehaviour
+{
+    private GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.gameObject;
+            player.transform.SetParent(this.transform);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.transform.SetParent(null);
+            player = null;
+        }
+    }
+}
