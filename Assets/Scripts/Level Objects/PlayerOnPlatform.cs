f69c96d83b4889f2db8b1144aff3b778b82f8f2c
JayHutter <jayhutter64@gmail.com>
2023-02-05 14:45:31 +0000

    Remove Player Parenting

--- Assets/Scripts/Level Objects/PlayerOnPlatform.cs
+++ Assets/Scripts/Level Objects/PlayerOnPlatform.cs
@@ -7,20 +7,20 @@ public class PlayerOnPlatform : MonoBehaviour
     private GameObject player;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            player = other.gameObject;
-            player.transform.SetParent(this.transform);
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    player = other.gameObject;
+        //    player.transform.SetParent(this.transform);
+        //}
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            player.transform.SetParent(null);
-            player = null;
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    player.transform.SetParent(null);
+        //    player = null;
+        //}
     }
 }
